44837b7765d9ee234c253e03ba02d1f0d89bf802
navishd <navishmitha.gunawardana2@gmail.com>
2025-09-11 16:06:10 +0530

    header final

---
+++ script.js
@@ -0,0 +1,21 @@
+ 'use strict';
+
+ const addEventOnElement= function(elements,evetType,callback){
+  for (let i= 0,len = elements.length;i<len;i++){
+    elements[i].addEventListener(eventType,callback);
+  }
+ }
+
+ /*navbar*/
+ const navbar= document.querySelector("[data-navbar]");
+ const navToggleBtn =document.querySelector("[data-nav-toggle-btn]");
+ const overlay = document.querySelector("[data-overlay]");
+
+ const toggleNavbar = function(){
+  navbar.classList.toggle("active");
+  navToggleBtn.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+ }
+
+ addEventOnElement([navToggleBtn,overlay,"click",toggleNavbar]);
